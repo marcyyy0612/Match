f8c76f72d856509ab1d364e26a2ed2d8da3c70ba
marcyyy0612 <marcyyy0612@gmail.com>
2019-03-20 01:07:51 +0900

    add httpclient into front

---
+++ angular/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  public getUsers(): void {
+    const url = 'http://localhost:9000/users';
+    this.http.get(url).subscribe(res => {
+      console.log(res);
+    });
+  }
+}
